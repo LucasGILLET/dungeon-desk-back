4a3fbae73a3402df934e634d1f0d3ad950b053d7
Lucas GILLET <lucasgillet852@gmail.com>
2026-02-27 12:59:54 +0100

    🐛 Ajout des migrations dans le pipeline ci/cd

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,11 +3,5 @@
 # Arrêter le script si une commande échoue
 set -e
 
-echo "Starting deployment script..."
-
-# Optionnel : Timeout pour éviter que le déploiement ne bloque indéfiniment si la DB n'est pas dispo
-# echo "Running migrations..."
-# npx prisma migrate deploy || echo "Migration failed, continuing anyway..."
-
 echo "Starting server..."
 exec "$@"
